1a4c8441a44146ad0829e6c3ea6c7d4d10e84af8
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-03 12:34:47 +0600

    added metarial ui card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import News from './components/news/News';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <Button variant="contained" color="primary">
+        hello
+      </Button>
+      <News></News>
     </div>
   );
 }
