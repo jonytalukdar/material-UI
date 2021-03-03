b7cbe4607c3935a218d14a0150ddf85cf8311ef6
jonytalukdar <jonytalukdar30@gmail.com>
2021-03-03 12:46:35 +0600

    added api news

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Button from '@material-ui/core/Button';
 import News from './components/news/News';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch(
+      'https://newsapi.org/v2/top-headlines?country=us&apiKey=270e491750014e89a47d15979365477a'
+    )
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  });
   return (
     <div className="App">
-      <h1>hello</h1>
       <Button variant="contained" color="primary">
         hello
       </Button>
